598bd46c99220c2e7299fcb64023caac66f96ec3
ShakedZrihen <shaked.zrihen@gmail.com>
2019-04-11 11:44:12 +0300

    CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 from config.db import app_db
@@ -11,6 +10,7 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config['CORS_ALLOW_HEADERS'] = "Content-Type"
+    app.config['CORS_HEADERS'] = "Content-Type"
     app.config['CORS_RESOURCES'] = {r"*": {"origins": "*"}}
     app.config['SQLALCHEMY_DATABASE_URI'] = app_db.DB_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
-from flask_cors import CORS
-
-from . import create_app
-
-app = create_app()
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-if __name__ == '__main__':
-   app.run()
+# from flask_cors import CORS
+#
+# from . import create_app
+#
+# app = create_app()
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+#
+# if __name__ == '__main__':
+#    app.run()
