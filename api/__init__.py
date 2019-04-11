1ed48b51549363a0757b678226c236d4345e5d2f
ShakedZrihen <shaked.zrihen@gmail.com>
2019-04-11 11:40:13 +0300

    Change CORS

--- api/__init__.py
+++ api/__init__.py
@@ -10,7 +10,8 @@ db = SQLAlchemy()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    app.config['CORS_ALLOW_HEADERS'] = "Content-Type"
+    app.config['CORS_RESOURCES'] = {r"/api/*": {"origins": "*"}}
     app.config['SQLALCHEMY_DATABASE_URI'] = app_db.DB_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
