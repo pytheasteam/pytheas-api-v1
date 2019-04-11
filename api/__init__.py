ddd03f1be821c336a18236ad1709ac4da1e78107
ShakedZrihen <shaked.zrihen@gmail.com>
2019-04-11 11:30:01 +0300

    Update CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 from config.db import app_db
 
@@ -10,10 +10,10 @@ db = SQLAlchemy()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
+    CORS(app, supports_credentials=True)
     app.config['SQLALCHEMY_DATABASE_URI'] = app_db.DB_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     with app.app_context():
         # Imports
         from . import routes
--- api/app.py
+++ api/app.py
@@ -1,37 +1,9 @@
-#
-#
-# @app.route('/api')
-# def hello_world():
-#     return 'App route!'
-#
-#
-# @app.route('/api/attractions')
-# def attractions():
-#     pass
-#
-#
-# @app.route('/api/cities')
-# def cities():
-#     pass
-#
-#
-# @app.route('/api/tags')
-# def tags():
-#     pass
-#
-#
-# @app.route('/api/trips')
-# def trips():
-#     pass
-#
-#
-# @app.route('/api/profile')
-# def profile():
-#     pass
-#
-#
-# @app.route('/api/explore')
-# def explore():
-#     pass
-#
+from flask_cors import CORS
 
+from . import create_app
+
+
+
+if __name__ == '__main__':
+    app = create_app()
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
