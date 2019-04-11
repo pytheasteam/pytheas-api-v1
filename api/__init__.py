1b0410d6f7303886b83c7038bbd68fc52d55b550
ShakedZrihen <shaked.zrihen@gmail.com>
2019-04-11 11:41:24 +0300

    CORS

--- api/__init__.py
+++ api/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config['CORS_ALLOW_HEADERS'] = "Content-Type"
-    app.config['CORS_RESOURCES'] = {r"/api/*": {"origins": "*"}}
+    app.config['CORS_RESOURCES'] = {r"*": {"origins": "*"}}
     app.config['SQLALCHEMY_DATABASE_URI'] = app_db.DB_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
