7e03ee8f773ea0713ecda2d524c093a1f49d2873
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-20 11:24:28 +0300

    Add resources to CORS

--- api/routes.py
+++ api/routes.py
@@ -7,7 +7,7 @@ from . import db
 
 db_manager = SQLPytheasManager(db)
 
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, resources={r'/*': {"origins": '*'}})
 
 
 @app.route('/')
