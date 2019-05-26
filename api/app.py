8b723e591336e57ea25d95f85e4befc4dc67b1e5
ShakedZrihen <shaked.zrihen@gmail.com>
2019-05-26 11:19:59 +0300

    Rename env vars

--- api/app.py
+++ api/app.py
@@ -2,8 +2,8 @@ from api import create_app
 import os
 
 app = create_app()
-PORT = os.environ.get('port', 5000)
-HOST = os.environ.get('host', '127.0.0.1')
+PORT = os.environ.get('PORT', 5000)
+HOST = os.environ.get('HOST', '127.0.0.1')
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT)
