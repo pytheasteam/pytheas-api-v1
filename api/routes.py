7bad007bd26e927cd7e186361c82003b30aef801
ShakedZrihen <shaked.zrihen@gmail.com>
2019-06-26 20:37:22 +0300

    Change CORS

--- api/routes.py
+++ api/routes.py
@@ -10,7 +10,6 @@ from . import db
 db_manager = SQLPytheasManager(db)
 
 CORS(app, supports_credentials=True, resources={r'/*': {"origins": '*', "headers": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
