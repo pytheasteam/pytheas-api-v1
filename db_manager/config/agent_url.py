e54afbfdd3dc1b6ceaea96d9628d6f7a60c9aaf9
ShakedZrihen <shaked.zrihen@gmail.com>
2019-06-28 23:05:39 +0300

    Update agent url

--- db_manager/config/agent_url.py
+++ db_manager/config/agent_url.py
@@ -1,3 +1,3 @@
-AGENT_ENDPOINT = 'http://ec2-35-180-196-241.eu-west-3.compute.amazonaws.com:5001/api'
+AGENT_ENDPOINT = 'http://ec2-35-180-196-241.eu-west-3.compute.amazonaws.com:5001/api/'
 AGENT_ATTRACTION_GET = 'get_attractions_for_profile'
 AGENT_TAGS_GET = 'get_tags'
